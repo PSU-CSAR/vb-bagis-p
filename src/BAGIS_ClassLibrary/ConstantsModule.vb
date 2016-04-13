be67ec5e1ba9fbbbcd3df8de8b96865b265302c5
Lesley <lbross@pdx.edu>
2016-04-13 16:47:12 -0700

    Work issue #21

--- src/BAGIS_ClassLibrary/ConstantsModule.vb
+++ src/BAGIS_ClassLibrary/ConstantsModule.vb
@@ -193,8 +193,8 @@
 
     'Constant parameter names for parameters calculated at the AOI-level
     Public Const BA_Aoi_Parameter_jh_coef As String = "jh_coef"
-    Public Const BA_Aoi_Parameter_SR_Obs As String = "SR_Obs"
-    Public Const BA_Aoi_Parameter_PE_Obs As String = "PE_Obs"
+    Public Const BA_Aoi_Parameter_SR_Obs As String = "SR"
+    Public Const BA_Aoi_Parameter_PE_Obs As String = "PET"
 
     'Constant prefix for BAGIS warning messages from GP
     Public Const BA_Warning_Message_Prefix As String = "BAGIS"
