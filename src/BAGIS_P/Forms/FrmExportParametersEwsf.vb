253bd36eae4f9b790003ba9185cfdbdc22b38c99
lbross <lbross@pdx.edu>
2020-11-20 13:38:14 -0800

    Change concat operator

--- src/BAGIS_P/Forms/FrmExportParametersEwsf.vb
+++ src/BAGIS_P/Forms/FrmExportParametersEwsf.vb
@@ -920,7 +920,7 @@ Public Class FrmExportParametersEwsf
             If Not String.IsNullOrEmpty(TxtDemResample.Text) Then
                 demCellSize = CDbl(TxtDemResample.Text)
             End If
-            Dim snapRasterPath As String = BA_GeodatabasePath(m_aoi.FilePath, GeodatabaseNames.Aoi, False) & BA_EnumDescription(PublicPath.AoiGrid)
+            Dim snapRasterPath As String = BA_GeodatabasePath(m_aoi.FilePath, GeodatabaseNames.Aoi, False) + BA_EnumDescription(PublicPath.AoiGrid)
 
             If demCellSize > 0 Then
                 'Need to resample
